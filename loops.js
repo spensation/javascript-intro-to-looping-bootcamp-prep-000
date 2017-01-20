3e1b035ffa9338a1d40c37a721495375e074714c
Spencer Evans <spencevans@gmail.com>
2017-01-20 23:53:50 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,11 +1,30 @@
-
-function forLoop() {
-  var i = 0
-  for(var i = 0; i < 25; i++) {
-    if i === 1 {
-      console.log("I am 1 strange loop.");
-    } else {
-      console.log("I am " + i + " strange loops.");
+function forLoop(array) {
+  for(let i = 1; i < 26; i++) {
+    if (i === 1) {
+      array.push ("I am 1 strange loop.");
+    } else if (i > 1 && i <= 26) {
+      array.push ("I am " + i + " strange loops.");
     }
   }
+  return array;
+}
+
+function whileLoop(n) {
+  while (n > 0) {
+    console.log(n);
+    n --;
+  }
+  return "done"
+}
+
+function doWhileLoop(array) {
+  function maybeTrue() {
+    return Math.random() >= 0.5
+   }
+
+  do {
+    array.shift()
+  }
+  while (array.length > 0 && maybeTrue());
+  return array;
 }
